a2817ca7732d288620d42685d5d9d455847199f4
Lucy Moore <122561103+lmoore36@users.noreply.github.com>
2024-08-02 09:08:48 -0700

    Update footer.jsx

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -7,16 +7,10 @@ const Footer = () => {
             © {new Date().getFullYear()} Lucy's Portfolio
           </p>
           <div className="social_icons">
-            <a
-              href="https://twitter.com/olawanle_joel"
-              aria-label="Twitter"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
               <i className="fa-brands fa-twitter"></i>
             </a>
             <a
-              href="https://github.com/olawanlejoel"
+              href="https://github.com/lmoore36"
               aria-label="GitHub"
               target="_blank"
               rel="noopener noreferrer"
@@ -24,7 +18,7 @@ const Footer = () => {
               <i className="fa-brands fa-github"></i>
             </a>
             <a
-              href="https://www.linkedin.com/in/olawanlejoel/"
+              href="https://www.linkedin.com/in/lucy-moore36/"
               aria-label="LinkedIn"
               target="_blank"
               rel="noopener noreferrer"
